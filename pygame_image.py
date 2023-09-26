7f4775b660ff6908aa03b70ded8dbef2496c445a
oshima <c0b220279c@edu.teu.ac.jp>
2023-09-26 15:58:19 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -15,11 +15,13 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        screen.blit(bg_img, [0, 0])
-        screen.blit(KK_imgs[tmr%2], [200, 300])
+        x = tmr%1600
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img, [1650-x, 0])
+        screen.blit(KK_imgs[tmr%2], [300, 200])
         pg.display.update()
-        tmr += 1        
-        clock.tick(10)
+        tmr += 1      
+        clock.tick(100)
         
 
 
